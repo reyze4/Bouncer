e8b5bc314bdbcafbf4d0c116e6d2e7186981dc79
262003 <262003@1305-010>
2023-06-09 12:40:31 +0300

    Добавьте файлы проекта.

---
+++ Components/PartialClass/ClientService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShcoolLearn.Components.Model
+{
+    public partial class ClientService
+    {
+        public string TimeConsole
+        {
+            get
+            {
+                var time = StartTime - DateTime.Now;
+                return $"Начало: {StartTime.ToString("F")}, осталось: {time.ToString(@"hh\:mm")}";
+            }
+        }
+
+        public string ColorTimes
+        {
+            get
+            {
+                if (StartTime - DateTime.Now < TimeSpan.FromHours(1))
+                {
+                    return "#ff0000";
+                }
+                else
+                {
+                    return "#ffffff";
+                }
+            }
+        }
+    }
+}
